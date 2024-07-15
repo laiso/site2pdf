c90de2173b760ee5e4564fbc34a37b648a02d389
laiso <laiso@lai.so>
2024-07-15 15:11:15 +0700

    fix: fail #2

--- bin/site2pdf.js
+++ bin/site2pdf.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import { spawnSync } from "node:child_process";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const args = process.argv.slice(2);
 
-const result = spawnSync("npx", ["tsx", "index.ts", ...args], {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const result = spawnSync("npx", ["tsx", path.resolve(__dirname, '../index.ts'), ...args], {
 	stdio: "inherit",
 });
 
